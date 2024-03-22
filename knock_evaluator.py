58c0d8ba54318c5a74b01913a7ce671599dbf9a2
NeuralBeat! <robinklett33@gmail.com>
2024-03-22 09:20:56 +0100

    knock evaluator methods

--- knock_evaluator.py
+++ knock_evaluator.py
@@ -56,3 +56,101 @@ def find_knock_event_ended(zBuffer, threshold, counter_threshold, prebuffer_size
                  return index+prebuffer_size # Return the current index if counter exceeds counter_threshold
         
      return None
+
+def calculate_ZImpactReturn(maxZ_Vel, maxX_Vel, maxY_Vel):
+    
+    result = False
+
+    conditionXZ = (2 * maxZ_Vel > 3 * maxX_Vel)
+    conditionYZ = (3 * maxZ_Vel > 8 * maxY_Vel)
+    # Evaluate the condition for ZImpactReturn
+    if conditionXZ and conditionYZ:
+        result = True
+
+    print('Quotient Z/X is big enough: ', conditionXZ,  ' ---> For positive Recognition required: True')
+    print('Quotient Z/Y is big enough: ', conditionYZ,  ' ---> For positive Recognition required: True')
+    print('ZImpactReturn: ', result,    '              ---> For positive Recognition required: True')
+    return result
+
+##### Calculate Very_High_Impact & update flag
+def calculate_Very_High_Impact(zBuffer, xBuffer_quasi_velocity, very_high_counter):
+    
+    result = False
+    # Convert to absolute values
+    abs_zBuffer = np.abs(zBuffer)
+    abs_xBuffer_quasi_velocity = np.abs(xBuffer_quasi_velocity)
+    
+    # (1) Check if more than 5 values in zBuffer are greater than 32767
+    condition1 = np.sum(abs_zBuffer > 32767) > very_high_counter
+    
+    # (2) Check if more than 5 values in xBuffer_quasi_velocity are greater than 16*11250
+    condition2 = np.sum(abs_xBuffer_quasi_velocity > (16 * 11250)) > very_high_counter
+    
+
+    if condition1 or condition2:
+        result = True
+    print('Very_High_Acc_Impact: ', condition1, '       ---> For positive Recognition required: False')
+    print('Very_High_Vel_Impact: ', condition2, '       ---> For positive Recognition required: False')
+    print('Very_High_Impact: ', result, '           ---> For positive Recognition required: False')
+
+    return result
+
+##### Calculate DeployFlag & update flag
+def calculate_DeployFlag(zBuffer, zBuffer_quasi_velocity, prebuffer_size, knock_event_ended, Acc_Latched_Counter, Vel_Latched_Counter, Acc_Latched_Threshold, Vel_Latched_Threshold):
+    
+    result = False
+    # Slice the input Series to the specified range
+    zBuffer_sliced = zBuffer[prebuffer_size:knock_event_ended]
+    zBuffer_quasi_velocity_sliced = zBuffer_quasi_velocity[prebuffer_size:knock_event_ended]
+    
+    # Convert to absolute values
+    zBuffer_abs = np.abs(zBuffer_sliced)
+    zBuffer_quasi_velocity_abs = np.abs(zBuffer_quasi_velocity_sliced)
+    
+    # Count values less than 8500
+    count_zBuffer = np.sum(zBuffer_abs < Acc_Latched_Threshold)
+    
+    count_zBuffer_quasi_velocity = np.sum(zBuffer_quasi_velocity_abs < Vel_Latched_Threshold)
+    
+
+    # Determine DeployFlag
+    if count_zBuffer > Acc_Latched_Counter or count_zBuffer_quasi_velocity > Vel_Latched_Counter:
+        result = False
+    
+    else:
+        result = True
+    
+    print('Acc_Latched_Counter: ', count_zBuffer, '           ---> For positive Recognition required: <', Acc_Latched_Counter)
+    print('Vel_Latched_Counter: ', count_zBuffer_quasi_velocity, '            ---> For positive Recognition required: <', Vel_Latched_Counter)
+    print('DeployFlag: ', result,  '                 ---> For positive Recognition required: True')
+    return [result, count_zBuffer, count_zBuffer_quasi_velocity]
+
+##### Calculate Has_It_Knocked & update flag
+def calculate_Has_It_Knocked(DeployFlag, Very_High_Impact, ZImpactReturn):
+
+    result = False
+
+    if DeployFlag and not Very_High_Impact and ZImpactReturn:
+        result = True
+
+    print('Knock Detected: ', result, '             ---> If this flag is True, then the knock has been detected')
+    return result
+
+
+def reshape_sequence(sequence):
+    """
+    Reshapes a flattened sequence array into a structured array with separate columns for x, y, z.
+    
+    Parameters:
+    - sequence: A flattened array of shape (270,) with the order x,y,z,x,y,z,...
+    
+    Returns:
+    - DataFrame with columns ['xBuffer', 'yBuffer', 'zBuffer']
+    """
+    # Reshape the sequence to have 3 columns
+    reshaped_sequence = sequence.reshape(-1, 3)
+    
+    # Create a DataFrame
+    df = pd.DataFrame(reshaped_sequence, columns=['xBuffer', 'yBuffer', 'zBuffer'])
+    
+    return df
